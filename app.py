7c2c055e494346ed59e70d17b18bd6750ea9cc34
Dan Callahan <dan.callahan@gmail.com>
2016-03-04 19:38:47 -0600

    Indent .py files with spaces, add .editorconfig
    If spaces are good enough for Guido, they're good enough for me.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 # configuration
@@ -9,29 +9,29 @@ app.config.from_object(__name__)
 
 
 class AuthException(Exception):
-	pass
+    pass
 
 
 @app.errorhandler(404)
 def hard_404(error):
-	return render_template("404.html"), 404
+    return render_template("404.html"), 404
 
 
 @app.route("/")
 def index():
-	return render_template("hello.html", text="Hello world")
+    return render_template("hello.html", text="Hello world")
 
 
 def main():
-	import sys
+    import sys
 
-	ip, port = "127.0.0.1", 5000
-	if len(sys.argv) > 1:
-		ip = sys.argv[1]
-		if len(sys.argv) > 2:
-			port = int(sys.argv[2])
-	app.run(ip, port)
+    ip, port = "127.0.0.1", 5000
+    if len(sys.argv) > 1:
+        ip = sys.argv[1]
+        if len(sys.argv) > 2:
+            port = int(sys.argv[2])
+    app.run(ip, port)
 
 
 if __name__ == "__main__":
-	main()
+    main()
